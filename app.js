403bd806b168456f91b3ea786784e46d4123f74c
Prosper Onungwa <onungwap@gmail.com>
2019-03-25 17:55:13 +0000

    comments routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
     seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});    
@@ -21,7 +22,7 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         } else{
-            res.render("index", {campgrounds: allCampgrounds});
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
         }
     })
     
@@ -44,7 +45,7 @@ app.post("/campgrounds", function(req, res){
 
 //NEW ROUTE
 app.get("/campgrounds/new", function(req, res) {
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 //SHOW ROUTE
@@ -53,11 +54,44 @@ app.get("/campgrounds/:id", function(req, res) {
        if(err){
            console.log(err);
        } else{
-           res.render("show", {campground: foundCampground});
+           res.render("campgrounds/show", {campground: foundCampground});
        }
     });
     
+});
+
+//COMMENTS ROUTES
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else{
+           res.render("comments/new", {campground: campground}); 
+        }
+    })
+    
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
+        }
+    })
 })
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running");
