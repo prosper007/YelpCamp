58e2131c856f9d9c6cc514bf561a893f2c33e9f8
Prosper Onungwa <onungwap@gmail.com>
2019-06-23 23:08:44 +0000

    update dburl

--- app.js
+++ app.js
@@ -19,9 +19,8 @@ var campgroundRoutes    = require("./routes/campgrounds"),
     commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index")
     
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
 
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});    
-mongoose.connect("mongodb://prosper:123456a@ds241977.mlab.com:41977/heroku_f7n43s4l", {useNewUrlParser: true});    
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
